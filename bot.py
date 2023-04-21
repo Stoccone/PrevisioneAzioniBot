18e1720a83746f26789f6baedf3797b81f057fd8
StockxN <106381166+StockxN@users.noreply.github.com>
2023-04-21 11:07:15 +0200

    push del bot

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
-ciao, prova
+import telebot
+
+bot = telebot.TeleBot("6248512780:AAHL0E69kmNatnoU-DBMZMJ_xBYM8C0WEUA")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+	bot.reply_to(message, "PORCODIO")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
