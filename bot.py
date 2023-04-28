d84684e51f89840bf9ff59db9dccfaebda68a93a
StockxN <106381166+StockxN@users.noreply.github.com>
2023-04-28 09:25:20 +0200

    kill bot

--- bot.py
+++ bot.py
@@ -4,11 +4,26 @@ bot = telebot.TeleBot("6248512780:AAHL0E69kmNatnoU-DBMZMJ_xBYM8C0WEUA")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Howdy, what do you want to do?")
+	# Aspetta l'input dell'utente
+	bot.register_next_step_handler(message, handle_input)
+# Gestisce l'input dell'utente
+def handle_input(message):
+    # Memorizza l'input dell'utente
+    input_text = message.text
+    bot.reply_to(message, "Ok allora: " + input_text)
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
 	bot.reply_to(message, "Ci siamo :)")
+
+	
+@bot.message_handler(commands=['kill'])
+def handle_stop(message):
+    # Interrompe il polling del bot
+	bot.reply_to(message, "Hai terminato questo bot :(")
+	bot.stop_polling()
+
 
 @bot.message_handler(func=lambda message: True)
  # Invia un messaggio di errore quando il bot riceve un comando non valido
@@ -16,3 +31,5 @@ def echo_all(message):
 	bot.reply_to(message, "Mi dispiace, non ho capito il comando che hai inviato.")
 
 bot.infinity_polling()
+
+
