3f280e108a934480c7d181f4c873bb103fdd7c42
StockxN <106381166+StockxN@users.noreply.github.com>
2023-05-09 18:37:10 +0200

    inserimento visione del grafico

--- bot.py
+++ bot.py
@@ -7,63 +7,98 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler 
 from keras.models import Sequential, load_model
 from keras.layers import Dense, LSTM 
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import yfinance as yf
+from telebot import types
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from datetime import datetime, timedelta
+
+from tensorflow.keras.models import load_model
 
 bot = telebot.TeleBot("6248512780:AAHL0E69kmNatnoU-DBMZMJ_xBYM8C0WEUA")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Questo è un bot sperimentale che fa schifo")
+	bot.reply_to(message, "Ciao, questo bot viene utilizzato per prevedere tramite un modello di Machine Learning il prezzo di una determinata azione :)")
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-	bot.reply_to(message, "Scrivci /prevedi")
+	bot.reply_to(message, "Per visualizzare il grafico di una determinata azione scrivi /view")
 
-@bot.message_handler(commands=['prevedi'])
 
+
+@bot.message_handler(commands=['view'])
 def send_welcome(message):
-	data = yf.download("AAPL", start="2022-1-1", end="2022-7-7").filter(['Close'])
-	dataset = data.values
+    bot.send_message(chat_id=message.chat.id, text="Inserisci un'azione da visualizzare:")
+    bot.register_next_step_handler(message, select_date_range)
 
-	scaler = MinMaxScaler(feature_range= (0,1))
-	scaled_data = scaler.fit_transform(dataset)
-	test_data = scaled_data[len(dataset) - 60: , :]
-	x_test = []
-	y_test = dataset
-	for i in range(60, len(test_data)) :
-		x_test.append(test_data[i-60:i, 0])
-	x_test = np.array(x_test)
-	x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
-	
-	model = load_model("aapl_model.h5")
-	predictions = model.predict(x_test)
-	predictions = scaler.inverse_transform(predictions)
+def select_date_range(message):
+    ticker = message.text.upper()
+    df = yf.download(ticker)
 
-	valid = data
-	valid['Predictions'] = predictions
-	plt.figure(figsize=(16,8))
-	plt.title('Model')
-	plt.xlabel('Date', fontsize=18)
-	plt.ylabel('Close Price USD ($)', fontsize=18)
-	plt.plot (valid[['Close', 'Predictions']])
-	plt.legend(['Val', 'Predictions'], loc='lower right')
+    years = df.index.year.unique()
+    year_buttons = [types.KeyboardButton(str(year)) for year in years]
+    year_markup = types.ReplyKeyboardMarkup(resize_keyboard=True).add(*year_buttons)
+    bot.send_message(chat_id=message.chat.id, text="Seleziona l'anno di inizio:", reply_markup=year_markup)
 
-	plt.savefig(buf, format='png')
-	buf = BytesIO()
-	buf.seek(0)
-	plt.close()
+    bot.register_next_step_handler(message, select_start_month, ticker, df)
 
-	bot.send_photo(chat_id=message.chat.id, photo=buf)
+def select_start_month(message, ticker, df):
+    selected_year = int(message.text)
+
+    months = [i.strftime('%B') for i in pd.date_range(start=f'{selected_year}-01-01', end=f'{selected_year}-12-01', freq='MS')]
+    month_buttons = [types.KeyboardButton(month) for month in months]
+    month_markup = types.ReplyKeyboardMarkup(resize_keyboard=True).add(*month_buttons)
+    bot.send_message(chat_id=message.chat.id, text='Seleziona il mese di inizio:', reply_markup=month_markup)
+
+    bot.register_next_step_handler(message, select_end_year, ticker, df, selected_year)
+
+def select_end_year(message, ticker, df, selected_start_year):
+    selected_start_month = message.text
+    selected_start_date = datetime.strptime(f'{selected_start_year}-{selected_start_month}-01', '%Y-%B-%d')
+
+    years = df.loc[selected_start_date:].index.year.unique()
+    year_buttons = [types.KeyboardButton(str(year)) for year in years]
+    year_markup = types.ReplyKeyboardMarkup(resize_keyboard=True).add(*year_buttons)
+    bot.send_message(chat_id=message.chat.id, text="Seleziona l'anno di fine:", reply_markup=year_markup)
+
+    bot.register_next_step_handler(message, select_end_month, ticker, df, selected_start_date)
+
+def select_end_month(message, ticker, df, selected_start_date):
+    selected_end_year = int(message.text)
+
+    months = [i.strftime('%B') for i in pd.date_range(start=f'{selected_end_year}-01-01', end=f'{selected_end_year}-12-01', freq='MS') if i >= selected_start_date]
+    month_buttons = [types.KeyboardButton(month) for month in months]
+    month_markup = types.ReplyKeyboardMarkup(resize_keyboard=True).add(*month_buttons)
+    bot.send_message(chat_id=message.chat.id, text='Seleziona il mese di fine:', reply_markup=month_markup)
+
+    bot.register_next_step_handler(message, generate_plot, ticker, df, selected_start_date, selected_end_year)
+
+def generate_plot(message, ticker, df, selected_start_date, selected_end_year):
+    selected_end_month = message.text
+    selected_end_date = datetime.strptime(f'{selected_end_year}-{selected_end_month}-01', '%Y-%B-%d')
 
 
-@bot.message_handler(commands=['stop'])
-def handle_stop(message):
-    # Interrompe il polling del bot
-    bot.stop_polling()
-	
 
-	
+    df = yf.download(ticker, start=selected_start_date, end=selected_end_date)
+
+    # Crea il grafico dello storico considerando solo la colonna close
+    plt.figure(figsize=(16,8))
+    plt.title('Close Price History')
+    plt.plot(df['Close'])
+    plt.xlabel('Date', fontsize=18)
+    plt.ylabel('Close Price USD ($)', fontsize=18) 
+
+    # Converte il grafico in un'immagine e la salva come buffer
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+
+    # Invia il grafico come foto
+    bot.send_photo(chat_id=message.chat.id, photo=buf)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
